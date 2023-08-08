a0533270ffaafdce3602318775e6f0bac3ce4826
Saif <saifm@marakeb.tech>
2023-08-08 11:56:44 +0400

    1- Added a new icon called "arrow1" through the missionplanner and missionplanner.maps resources
    - To access the missionplanner.maps resources , you'll have to go to one of its files (like for example GMapMarkerRover) then go to project --> missionplanner.maps properties and add your icon there in the resources
    
    2- Replaced the rover icon with the new "arrow1" that was added
    
    - Changed in the GMapMarkerRover the string "rover" to be "arrow1" after adding "arrow1" to the MissionPlanner.Maps resources property

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -273,6 +273,16 @@ namespace MissionPlanner.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        public static System.Drawing.Bitmap arrow1 {
+            get {
+                object obj = ResourceManager.GetObject("arrow1", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         public static System.Drawing.Bitmap attocurrent {
             get {
                 object obj = ResourceManager.GetObject("attocurrent", resourceCulture);
