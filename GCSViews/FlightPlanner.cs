888deca5f21691481858ee92ef5647e54c4eb463
44amh <alimouneer@marakeb.tech>
2023-11-07 11:08:18 +0400

    commands i

--- GCSViews/FlightPlanner.cs
+++ GCSViews/FlightPlanner.cs
@@ -5434,7 +5434,7 @@ namespace MissionPlanner.GCSViews
             Commands.Enabled = false;
 
            // int i = Commands.Rows.Count - 1;
-            int i = Commands.Rows.Count;
+            int i =0;
             int cmdidx = -1;
             foreach (Locationwp temp in cmds)
             {
@@ -8470,8 +8470,88 @@ Column 1: Field type (RALLY is the only one at the moment -- may have RALLY_LAND
         private void BUT_write_Click_1(object sender, EventArgs e)
         {
             panel10.Visible = false;
-            uploadwps();
-        }
+                if ((altmode)CMB_altmode.SelectedValue == altmode.Absolute)
+                {
+                    if ((int)DialogResult.No ==
+                        CustomMessageBox.Show("Absolute Alt is selected are you sure?", "Alt Mode",
+                            MessageBoxButtons.YesNo))
+                    {
+                        CMB_altmode.SelectedValue = (int)altmode.Relative;
+                    }
+                }
+
+                // check home
+                Locationwp home = new Locationwp();
+                try
+                {
+                    home.frame = (byte)MAVLink.MAV_FRAME.GLOBAL;
+                    home.id = (ushort)MAVLink.MAV_CMD.WAYPOINT;
+                    home.lat = (double.Parse(TXT_homelat.Text));
+                    home.lng = (double.Parse(TXT_homelng.Text));
+                    home.alt = (float.Parse(TXT_homealt.Text) / CurrentState.multiplierdist); // use saved home
+                }
+                catch
+                {
+                    CustomMessageBox.Show("Your home location is invalid", Strings.ERROR);
+                    return;
+                }
+
+                // check for invalid grid data
+                for (int a = 0; a < Commands.Rows.Count - 0; a++)
+                {
+                    for (int b = 0; b < Commands.ColumnCount - 0; b++)
+                    {
+                        double answer;
+                        if (b >= 1 && b <= 7)
+                        {
+                            if (!double.TryParse(Commands[b, a].Value.ToString(), out answer))
+                            {
+                                CustomMessageBox.Show("There are errors in your mission");
+                                return;
+                            }
+                        }
+
+                        if (TXT_altwarn.Text == "") TXT_altwarn.Text = (0).ToString();
+
+                        if (Commands.Rows[a].Cells[Command.Index].Value.ToString().Contains("UNKNOWN"))
+                            continue;
+
+                        ushort cmd = getCmdID(Commands.Rows[a].Cells[Command.Index].Value.ToString());
+
+                        if (cmd < (ushort)MAVLink.MAV_CMD.LAST &&
+                            double.Parse(Commands[Alt.Index, a].Value.ToString()) < double.Parse(TXT_altwarn.Text))
+                        {
+                            if (cmd != (ushort)MAVLink.MAV_CMD.TAKEOFF &&
+                                cmd != (ushort)MAVLink.MAV_CMD.LAND &&
+                                cmd != (ushort)MAVLink.MAV_CMD.RETURN_TO_LAUNCH)
+                            {
+                                CustomMessageBox.Show("Low alt on WP#" + (a + 1) +
+                                                      "\nPlease reduce the alt warning, or increase the altitude");
+                                return;
+                            }
+                        }
+                    }
+                }
+
+                IProgressReporterDialogue frmProgressReporter = new ProgressReporterDialogue
+                {
+                    StartPosition = FormStartPosition.CenterScreen,
+                    Text = "Sending WP's"
+                };
+
+                frmProgressReporter.DoWork += saveWPs;
+
+                frmProgressReporter.UpdateProgressAndStatus(-1, "Sending WP's");
+
+                ThemeManager.ApplyThemeTo(frmProgressReporter);
+
+                frmProgressReporter.RunBackgroundOperationAsync();
+
+                frmProgressReporter.Dispose();
+
+                MainMap.Focus();
+            }
+        
 
         private void myButton21_Click(object sender, EventArgs e)
         {
